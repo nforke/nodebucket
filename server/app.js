20160ad915f7e6c19af74e20c745892b9b279c77
nforke <n.forke@gmail.com>
2020-10-16 20:17:08 -0500

    update server file

--- server/app.js
+++ server/app.js
@@ -33,11 +33,7 @@ app.use('/', express.static(path.join(__dirname, '../dist/nodebucket')));
 /**
  * Variables
  */
-<<<<<<< HEAD
-const port = process.env.PORT || 3000; // server port
-=======
 const port = env.process.PORT || 3000; // server port for application
->>>>>>> dev-branch
 
 // TODO: This line will need to be replaced with your actual database connection string
 const conn = 'mongodb+srv://nodebucket:ohBuckets@buwebdev-cluster-1.bznkj.mongodb.net/nodebucket';
